1b49acb0410e65c05c82426ce1c2a6665956ac99
arnelagutlicc <135592870+arnelagutlicc@users.noreply.github.com>
2026-06-18 09:36:40 +0200

    Update CouponController.php

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -21,4 +21,18 @@ class CouponController extends Controller
     public function update(Request $request, Coupon $coupon) {}
 
     public function destroy(Coupon $coupon) {}
+
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function show(Coupon $coupon) {}
+
+    public function edit(Coupon $coupon) {}
+
+    public function update(Request $request, Coupon $coupon) {}
+
+    public function destroy(Coupon $coupon) {}
 }
