60547c82ef92f39483a76b9f3d190f1c5ceddc76
arnelagutlicc <135592870+arnelagutlicc@users.noreply.github.com>
2026-06-22 11:23:02 +0200

    Update ReturnStatusController.php fix AR-4

--- app/Http/Controllers/Admin/ReturnStatusController.php
+++ app/Http/Controllers/Admin/ReturnStatusController.php
@@ -20,3 +20,18 @@ class ReturnStatusController extends Controller
 
     public function destroy(OrderReturnStatus $orderReturnStatus) {}
 }
+
+class ReturnStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderReturnStatus $orderReturnStatus) {}
+
+    public function update(Request $request, OrderReturnStatus $orderReturnStatus) {}
+
+    public function destroy(OrderReturnStatus $orderReturnStatus) {}
+}
