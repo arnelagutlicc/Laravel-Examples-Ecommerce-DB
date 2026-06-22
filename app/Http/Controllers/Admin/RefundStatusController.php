9728ed1e11cca60f41e54b05b242797b9b03e975
arnelagutlicc <135592870+arnelagutlicc@users.noreply.github.com>
2026-06-22 11:30:56 +0200

    Update RefundStatusController.php fix AR-9

--- app/Http/Controllers/Admin/RefundStatusController.php
+++ app/Http/Controllers/Admin/RefundStatusController.php
@@ -20,3 +20,18 @@ class RefundStatusController extends Controller
 
     public function destroy(OrderRefundStatus $orderRefundStatus) {}
 }
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
