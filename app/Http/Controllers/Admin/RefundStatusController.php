e5299709606789a7364083eb3955231972de134d
arnelagutlicc <135592870+arnelagutlicc@users.noreply.github.com>
2026-06-25 15:34:11 +0200

    Update RefundStatusController.php

--- app/Http/Controllers/Admin/RefundStatusController.php
+++ app/Http/Controllers/Admin/RefundStatusController.php
@@ -35,3 +35,183 @@ class RefundStatusController1 extends Controller
 
     public function destroy(OrderRefundStatus $orderRefundStatus) {}
 }
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
+
+class RefundStatusController1 extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit(OrderRefundStatus $orderRefundStatus) {}
+
+    public function update(Request $request, OrderRefundStatus $orderRefundStatus) {}
+
+    public function destroy(OrderRefundStatus $orderRefundStatus) {}
+}
